cca193a11baa3f40b550f89ee2f90fc4ee9e6751
CaptainIllidan <37904051+CaptainIllidan@users.noreply.github.com>
2019-05-23 14:21:20 +0400

    993

---
+++ ConventionsAndConstraints/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ConventionsAndConstraints.Infrastructure;
+using ConventionsAndConstraints.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConventionsAndConstraints.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+
+        [ActionNamePrefix("Do")]
+        public IActionResult List() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(List)
+        });
+    }
+}
